10e16bf523bea0fec795083db117c3a857092f8c
junxiang <kingjun15@126.com>
2018-02-27 15:57:08 +0800

    problem 1000 add

---
+++ 1000/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  1000
+//
+//  Created by junxiang on 2/27/18.
+//  Copyright © 2018 junxiang. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    freopen("in.in", "r", stdin);
+    int a, b;    
+    while (cin>>a>>b) {
+        cout<<a+b<<endl;
+    }    
+    return 0;
+}
